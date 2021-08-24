a41459f0292a7b7a32626b3fae47a28d6e8cc2de
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-08-24 16:03:05 -0700

    start of GoogleAuth component and loading a component did mount function to initialize the google api library at load

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,6 +1,15 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
+  componentDidMount() {
+    window.gapi.load('client:auth2', () => {
+      window.gapi.client.init({
+        clientId: process.env.REACT_APP_OAUTH_CLIENT_ID,
+        scope: 'email'
+      });
+    });
+  }
+
   render () {
     return (
       <div>Google Auth</div>

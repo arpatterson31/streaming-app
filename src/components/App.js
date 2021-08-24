7cc7f1e16457241eab3a6400566b345718aef838
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-08-24 15:15:29 -0700

    brought in header to app file

--- src/components/App.js
+++ src/components/App.js
@@ -6,18 +6,23 @@ import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
+import Header from './Header';
 
 const App = () => {
   return (
-    <BrowserRouter>
     <div>
-     <Route path="/" exact component={StreamList} />
-     <Route path="/streams/new" exact component={StreamCreate} />
-     <Route path="/streams/edit" exact component={StreamEdit} />
-     <Route path="/streams/delete" exact component={StreamDelete} />
-     <Route path="/streams/show" exact component={StreamShow} />
+      <Header />
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
+        </div>
+      </BrowserRouter>
+
     </div>
-    </BrowserRouter>
   );
 };
 

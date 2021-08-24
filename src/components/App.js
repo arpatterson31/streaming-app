12fe43eaa0fa2828c8ef2618b7db2066e66f43cc
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-08-24 15:21:48 -0700

    cleaned up import statements

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import StreamCreate from './streams/StreamCreate';
 import StreamEdit from './streams/StreamEdit';

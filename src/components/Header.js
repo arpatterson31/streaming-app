8cafade2a01f47bde963809a7ae8406943ff3e86
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-08-24 15:21:23 -0700

    built out header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <div>
-      Header
+    <div className="ui secondary pointing menu">
+      <Link to="/" className="item">
+        Streamin'
+      </Link>
+      <div className="right menu">
+        <Link to="/" className="item">
+          All Streams
+        </Link>
+      </div>
     </div>
   );
 };

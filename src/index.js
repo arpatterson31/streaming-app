a6831a382df4cb412d1bc37142c98c858a33d5a5
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-08-24 21:28:53 -0700

    redux basic setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 import App from './components/App';
+import reducers from './reducers';
+
+const store = createStore(reducers);
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
 

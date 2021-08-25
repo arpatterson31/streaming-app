ed15160852463ad62ffef5771c09883dba6438ff
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-08-24 21:41:18 -0700

    added auth reducer to combine reducer file

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
+import authReducer from './authReducer';
 
 export default combineReducers({
-  replaceMe: () => 'hey girl hey!'
+  auth: authReducer
 });
 

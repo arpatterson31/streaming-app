28086e01d099a1c33b28cd4555f5364e52aea18a
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-08-25 15:41:17 -0700

    bringing in thunk

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
@@ -11,7 +12,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   reducers,
-  composeEnhancers(applyMiddleware()) // activates thunk and redux devtools
+  composeEnhancers(applyMiddleware(thunk)) // activates thunk and redux devtools
   );
 
 ReactDOM.render(

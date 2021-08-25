2052bda3236b871cb26e5c7978c36ae174df32a5
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-08-25 13:45:10 -0700

    brought in redux devtools

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 
 import App from './components/App';
 import reducers from './reducers';
 
-const store = createStore(reducers);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// activates redux devtools
+
+const store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware()) // activates thunk and redux devtools
+  );
 
 ReactDOM.render(
   <Provider store={store}>

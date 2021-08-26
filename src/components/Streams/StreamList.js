e4cc2d27ce48398acf6a77536238e13e378c36db
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-08-26 12:20:12 -0700

    created a link to create form and added a button to show only if user is logged in

--- src/components/Streams/StreamList.js
+++ src/components/Streams/StreamList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { fetchStreams } from '../../actions';
 
@@ -7,7 +8,7 @@ class StreamList extends React.Component {
     this.props.fetchStreams();
   }
 
-  renderAdmin(stream) {
+  renderAdminButtons = (stream) => {
     if (stream.userId === this.props.currentUserId) {
       return (
         <div className="right floated content">
@@ -18,11 +19,11 @@ class StreamList extends React.Component {
     }
   }
 
-  renderList() {
+  renderList = () => {
     return this.props.streams.map(stream => {
       return (
         <div className="item" key={stream.id}>
-          {this.renderAdmin(stream)}
+          {this.renderAdminButtons(stream)}
           <i className="large middle aligned icon camera" />
           <div className="content">
             {stream.title}
@@ -34,11 +35,22 @@ class StreamList extends React.Component {
     });
   }
 
+  renderCreateButton = () => {
+    if(this.props.isSignedIn) {
+      return (
+        <div style={{ textAlign: 'right' }}>
+          <Link to="/streams/new" className="ui button primary">Create Stream</Link>
+        </div>
+      )
+    }
+  }
+
   render() {
     return (
       <div>
         <h2>Streams</h2>
         <div className="ui celled list">{this.renderList()}</div>
+        {this.renderCreateButton()}
       </div>
     )
   }
@@ -47,7 +59,8 @@ class StreamList extends React.Component {
 const mapStateToProps = (state) => {
   return { 
     streams: Object.values(state.streams),
-    currentUserId: state.auth.userId 
+    currentUserId: state.auth.userId,
+    isSignedIn: state.auth.isSignedIn
   }
 }
 
